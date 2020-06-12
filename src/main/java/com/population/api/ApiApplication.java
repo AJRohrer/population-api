921a2487e17c3c3808e356f765f7469b7aa16f0e
Andrew Rohrer <andrewjrohrer@gmail.com>
2020-06-11 21:00:00 -0500

    Request to API is working

--- src/main/java/com/population/api/ApiApplication.java
+++ src/main/java/com/population/api/ApiApplication.java
@@ -2,6 +2,7 @@ package com.population.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +10,7 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/com/population/api/PopulationController.java
+++ src/main/java/com/population/api/PopulationController.java
@@ -1,9 +1,25 @@
 package com.population.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping(path = "/")
 public class PopulationController {
 
-    public PopulationController(){}
+    @CrossOrigin(origins = "*")
+    @GetMapping("/hello")
+    public String helloWorld(@RequestParam String teststring){
+        return "This is the test string: " + teststring;
+    }
+
+    @CrossOrigin(origins="*")
+    @GetMapping("/populationestimate")
+    public String getPopulationEstimates(){
+        final String requestUrl = "https://api.census.gov/data/2019/pep/population?get=DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=region:*&key=aa32a07a49de32f2d38267f826a084a1c3030221";
+        RestTemplate rt = new RestTemplate();
+        Object result = rt.getForObject(requestUrl, Object.class);
+        return null;
+    }
 }
