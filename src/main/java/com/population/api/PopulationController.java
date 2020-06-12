8a1662c357fd20e5af8d4a5530f4efa6b4c555da
Andrew Rohrer <andrewjrohrer@gmail.com>
2020-06-11 19:43:10 -0500

    initial commit

---
+++ src/main/java/com/population/api/PopulationController.java
@@ -0,0 +1,9 @@
+package com.population.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PopulationController {
+
+    public PopulationController(){}
+}
